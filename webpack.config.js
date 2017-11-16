60afcd2effb443354de72422c2d7110c926953dc
Brian Macdonald <brian.lmacdonald@gmail.com>
2017-11-16 16:01:16 -0500

    multiplayer

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './client/index.js',
   output: {
     path: __dirname,
-    filename: './public/bundle.js'
+    filename: './public/part1.js'
   },
   devtool: 'source-map',
   module: {
