628b1d859622450fd313a06fa2fc5ef8f28b0aaa
danielcruser <danielcruser1@gmail.com>
2017-12-02 19:01:37 -0500

    removing db/passport

--- server/index.js
+++ server/index.js
@@ -3,11 +3,11 @@ const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const compression = require('compression')
-const session = require('express-session')
-const passport = require('passport')
-const SequelizeStore = require('connect-session-sequelize')(session.Store)
-const db = require('./db')
-const sessionStore = new SequelizeStore({db})
+// const session = require('express-session')
+// const passport = require('passport')
+// const SequelizeStore = require('connect-session-sequelize')(session.Store)
+// const db = require('./db')
+// const sessionStore = new SequelizeStore({db})
 const PORT = process.env.PORT || 8080
 const app = express()
 const socketio = require('socket.io')
@@ -24,11 +24,11 @@ module.exports = app
 if (process.env.NODE_ENV !== 'production') require('../secrets')
 
 // passport registration
-passport.serializeUser((user, done) => done(null, user.id))
-passport.deserializeUser((id, done) =>
-  db.models.user.findById(id)
-    .then(user => done(null, user))
-    .catch(done))
+// passport.serializeUser((user, done) => done(null, user.id))
+// passport.deserializeUser((id, done) =>
+//   db.models.user.findById(id)
+//     .then(user => done(null, user))
+//     .catch(done))
 
 const createApp = () => {
   // logging middleware
@@ -42,14 +42,14 @@ const createApp = () => {
   app.use(compression())
 
   // session middleware with passport
-  app.use(session({
-    secret: process.env.SESSION_SECRET || 'my best friend is Cody',
-    store: sessionStore,
-    resave: false,
-    saveUninitialized: false
-  }))
-  app.use(passport.initialize())
-  app.use(passport.session())
+  // app.use(session({
+  //   secret: process.env.SESSION_SECRET || 'my best friend is Cody',
+  //   store: sessionStore,
+  //   resave: false,
+  //   saveUninitialized: false
+  // }))
+  // app.use(passport.initialize())
+  // app.use(passport.session())
 
   // auth and api routes
   app.use('/auth', require('./auth'))
