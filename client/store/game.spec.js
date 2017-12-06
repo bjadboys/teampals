b3edd4f51e150e716c9223683ecd0326aad46229
Brian Macdonald <brian.lmacdonald@gmail.com>
2017-12-06 12:06:52 -0500

    adds game store testing suite

--- client/store/game.spec.js
+++ client/store/game.spec.js
@@ -1,48 +1,46 @@
 /* global describe beforeEach afterEach it */
+import gameReducer, {actionTypesForTesting} from './game'
 
-import {expect} from 'chai'
-import axios from 'axios'
-import configureMockStore from 'redux-mock-store'
-import thunkMiddleware from 'redux-thunk'
-import history from '../history'
-
-const middlewares = [thunkMiddleware]
-const mockStore = configureMockStore(middlewares)
-
-describe('thunk creators', () => {
-  let store
+describe('Game', () => {
 
   const initialState = {localGame: false, serverGame: false, joined: false, lobbyFull: false}
+  const gameInProgressJoinedState = { localGame: false, serverGame: true, joined: true, lobbyFull: false }
+  const localGameState = { localGame: true, serverGame: true, joined: true, lobbyFull: false }
+  const joinedState = { localGame: false, serverGame: false, joined: true, lobbyFull: false }
+  const lobbyFullState = { localGame: false, serverGame: false, joined: false, lobbyFull: true }
+  const gameInProgressState = { localGame: false, serverGame: true, joined: false, lobbyFull: false }
 
-  beforeEach(() => {
-    store = mockStore(initialState)
-  })
+  describe('Game reducer', () => {
+    it('should return the initial state', () => {
+      expect(gameReducer(undefined, initialState)).toEqual(initialState)
+    })
+    
+      it('should return true if a player has joined', () => {
+        expect(gameReducer(initialState, {type: actionTypesForTesting.JOINED_GAME})).toEqual(joinedState)
+      })
+  
+    
+      it('should return true if a player has tried to join a full lobby', () => {
+        expect(gameReducer(initialState, {type: actionTypesForTesting.LOBBY_FULL})).toEqual(lobbyFullState)
+      })
 
-  afterEach(() => {
-    store.clearActions()
-  })
+    it('should return false if a player has left the lobby', () => {
+      expect(gameReducer(joinedState, {type: actionTypesForTesting.LEFT_GAME})).toEqual(initialState)
+    })
 
-  describe('Game store', () => {
-    it('marks a player as joined when they join a game', () => {
-      const fakeUser = {email: 'Cody'}
-      return store.dispatch()
-        .then(() => {
-          const actions = store.getActions()
-          expect(actions[0].type).to.be.equal('GET_USER')
-          expect(actions[0].user).to.be.deep.equal(fakeUser)
-        })
+    
+    it('should return serverGame true if a game is in progress', () => {
+      expect(gameReducer(initialState, {type: actionTypesForTesting.GAME_IN_PROGRESS})).toEqual(gameInProgressState)
+    })
+    
+    it('should return serverGame true if a game is in progress', () => {
+      expect(gameReducer(gameInProgressJoinedState, { type: actionTypesForTesting.START_GAME })).toEqual(localGameState)
+    })
+
+    it('should reset game state if the game is over', () => {
+      expect(gameReducer(localGameState, {type: actionTypesForTesting.GAME_OVER})).toEqual(initialState)
     })
   })
 
-  describe('logout', () => {
-    it('logout: eventually dispatches the REMOVE_USER action', () => {
-      mockAxios.onPost('/auth/logout').replyOnce(204)
-      return store.dispatch(logout())
-        .then(() => {
-          const actions = store.getActions()
-          expect(actions[0].type).to.be.equal('REMOVE_USER')
-          expect(history.location.pathname).to.be.equal('/login')
-        })
-    })
-  })
+
 })
